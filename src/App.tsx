e90c7b2c44564594892560bd70a56e45d75cf8d9
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2024-11-15 20:00:11 +0530

    :rocket: Router added

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes,Link} from "react-router-dom";
+import './App.css'
+import ReactTourDemo from './pages/Reactour'
+import WalkTourDemo from './pages/Walktour'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={
+          <div>
+          <h3>Select a tour to view:</h3>
+          <Link to="/react-tour"><button style={{ marginRight: "10px" }}>View React Tour</button> </Link>
+          <Link to="/walk-tour"><button>View Walk Tour</button></Link>
+        </div>
+        }/>
+        <Route path='/react-tour' element={<ReactTourDemo/>}/>
+        <Route path='/walk-tour' element={<WalkTourDemo/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
